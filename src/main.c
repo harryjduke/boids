b3442848a818009e52290c32041110c3cb8a4a4e
harryjduke <harryjduke@gmail.com>
2025-11-07 02:28:19 +0000

    Basic random spawning

--- src/main.c
+++ src/main.c
@@ -4,10 +4,21 @@
 
 #include "raylib.h"
 
+void spawnBoids(Vector2* boidsArray, int numberOfBoids, Vector2 spawnPoint, Vector2 spawnBounds) {
+	for (int i = 0; i < numberOfBoids; i++) {
+		boidsArray[i] = (Vector2){GetRandomValue(spawnPoint.x, spawnBounds.x), GetRandomValue(spawnPoint.y, spawnBounds.y)};
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	const int screenWidth = 800;
 	const int screenHeight = 450;
+
+	const int numberOfBoids = 10;
+
+	Vector2 boids[numberOfBoids];
+	spawnBoids(boids, numberOfBoids, (Vector2){0, 0}, (Vector2){screenWidth, screenHeight});
 
 	InitWindow(screenWidth, screenHeight, "Boids");
 	SetTargetFPS(60);
@@ -18,6 +29,11 @@ int main(int argc, char *argv[])
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
+
+		for (int i = 0; i < numberOfBoids; i++) {
+			DrawCircle(boids[i].x, boids[i].y, 5.f, MAROON);
+		}
+
 		DrawText("Boids", 10, 10, 20, DARKGRAY);
 		
 		EndDrawing();
