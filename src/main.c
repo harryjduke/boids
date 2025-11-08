dcf3f626a82cb7a9db9abefb5178ff8ff414de9b
harryjduke <harryjduke@gmail.com>
2025-11-08 02:22:02 +0000

    Updated .gitignore and changed indents from tabs to spaces

--- src/main.c
+++ src/main.c
@@ -4,41 +4,42 @@
 
 #include "raylib.h"
 
-void spawnBoids(Vector2* boidsArray, int numberOfBoids, Vector2 spawnPoint, Vector2 spawnBounds) {
-	for (int i = 0; i < numberOfBoids; i++) {
-		boidsArray[i] = (Vector2){GetRandomValue(spawnPoint.x, spawnBounds.x), GetRandomValue(spawnPoint.y, spawnBounds.y)};
-	}
+void spawnBoids(Vector2 *boidsArray, int numberOfBoids, Rectangle spawnBounds) {
+    for (int i = 0; i < numberOfBoids; i++) {
+        boidsArray[i] = (Vector2){GetRandomValue(spawnBounds.x, spawnBounds.width),
+                                  GetRandomValue(spawnBounds.y, spawnBounds.height)};
+        return;
+    }
 }
 
-int main(int argc, char *argv[])
-{
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+int main(int argc, char *argv[]) {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-	const int numberOfBoids = 10;
+    const int numberOfBoids = 10;
 
-	Vector2 boids[numberOfBoids];
-	spawnBoids(boids, numberOfBoids, (Vector2){0, 0}, (Vector2){screenWidth, screenHeight});
+    Vector2 boids[numberOfBoids];
+    spawnBoids(boids, numberOfBoids, (Rectangle){0.f, 0.f, screenWidth, screenHeight});
 
-	InitWindow(screenWidth, screenHeight, "Boids");
-	SetTargetFPS(60);
+    InitWindow(screenWidth, screenHeight, "Boids");
+    SetTargetFPS(60);
 
-	while (!WindowShouldClose()) {
+    while (!WindowShouldClose()) {
 
-		// Draw
-		BeginDrawing();
+        // Draw
+        BeginDrawing();
 
-		ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-		for (int i = 0; i < numberOfBoids; i++) {
-			DrawCircle(boids[i].x, boids[i].y, 5.f, MAROON);
-		}
+        for (int i = 0; i < numberOfBoids; i++) {
+            DrawCircle(boids[i].x, boids[i].y, 5.f, MAROON);
+        }
 
-		DrawText("Boids", 10, 10, 20, DARKGRAY);
-		
-		EndDrawing();
-	}
+        DrawText("Boids", 10, 10, 20, DARKGRAY);
 
-	CloseWindow();
-	return EXIT_SUCCESS;
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return EXIT_SUCCESS;
 }
